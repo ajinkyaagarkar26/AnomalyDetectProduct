8b3368c01b660543de29ee954926efbeb0f80f53
ajinkya agarkar <ajinkya.agarkar@nexthink.com>
2025-07-23 13:08:10 +0530

    updated logs

--- test_sequence_prediction.py
+++ test_sequence_prediction.py
@@ -13,35 +13,7 @@ def test_sequence_prediction():
     """Test the sequence prediction endpoint with sample data"""
     
     # Test cases
-    test_cases = [
-        # {
-        #     "name": "Normal sequence with integers",
-        #     "data": {
-        #         "event_sequence": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
-        #         "description": "Normal sequential events"
-        #     }
-        # },
-        # {
-        #     "name": "Short sequence",
-        #     "data": {
-        #         "event_sequence": [1, 2, 3],
-        #         "description": "Short sequence - should be padded"
-        #     }
-        # },
-        # {
-        #     "name": "Long sequence",
-        #     "data": {
-        #         "event_sequence": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15],
-        #         "description": "Long sequence - should use sliding windows"
-        #     }
-        # },
-        # {
-        #     "name": "Potentially anomalous sequence",
-        #     "data": {
-        #         "event_sequence": [1, 1, 1, 1, 1, 23, 23, 23, 23, 23],
-        #         "description": "Repeated unusual events"
-        #     }
-        # },
+    test_cases = [                
         {
             "name": "Raw log lines sequence",
             "data": {
@@ -50,20 +22,124 @@ def test_sequence_prediction():
                     "INFO nova.virt.libvirt.driver [req-a4498d64-47bb-491f-adde-effccaba43f0] [instance: 11760334-ac63-4cc8-9086-578422af8c99] Creating image",
                     "INFO os_vif [req-a4498d64-47bb-491f-adde-effccaba43f0] Successfully plugged vif VIFOpenVSwitch",
                     "INFO nova.compute.manager [req-ac9f5721-5c52-4ec3-ba8a-e494d9780d53] [instance: 11760334-ac63-4cc8-9086-578422af8c99] VM Started (Lifecycle Event)",
+                    "INFO nova.virt.libvirt.driver [-] [instance: 11760334-ac63-4cc8-9086-578422af8c99] Instance spawned successfully.",
+                    "INFO nova.compute.manager [req-a4498d64-47bb-491f-adde-effccaba43f0] [instance: 11760334-ac63-4cc8-9086-578422af8c99] Took 9.31 seconds to spawn the instance on the hypervisor.",
+                    "INFO nova.compute.manager,[req-ac9f5721-5c52-4ec3-ba8a-e494d9780d53],[instance: 11760334-ac63-4cc8-9086-578422af8c99] VM Paused (Lifecycle Event)",
+                    "INFO nova.compute.manager [req-ac9f5721-5c52-4ec3-ba8a-e494d9780d53] [instance: 11760334-ac63-4cc8-9086-578422af8c99] VM Resumed (Lifecycle Event)",
+                    "WARNING nova.virt.libvirt.driver [req-32833659-9280-430c-b915-a246f788ad28] Error from libvirt while getting description of instance-00000072",
                     "ERROR nova.compute.manager [req-ac9f5721-5c52-4ec3-ba8a-e494d9780d53] [instance: 11760334-ac63-4cc8-9086-578422af8c99] Unknown error occurred"
                 ],
                 "description": "Raw log lines with potential anomaly",
                 "input_type": "raw_logs"
             }
+        },
+        {
+            "name": "Real nova log sequence from sample file",
+            "data": {
+                "event_sequence": [
+                    "INFO nova.compute.manager [req-ac9f5721-5c52-4ec3-ba8a-e494d9780d53] [instance: 2e0f047e-8d2f-4166-a950-ecf24c8743a7] VM Started (Lifecycle Event)",
+                    "INFO nova.compute.manager [req-ac9f5721-5c52-4ec3-ba8a-e494d9780d53] [instance: 2e0f047e-8d2f-4166-a950-ecf24c8743a7] VM Paused (Lifecycle Event)",
+                    "INFO nova.compute.manager [req-ac9f5721-5c52-4ec3-ba8a-e494d9780d53] [instance: 2e0f047e-8d2f-4166-a950-ecf24c8743a7] During sync_power_state the instance has a pending task (spawning). Skip.",
+                    "INFO nova.virt.libvirt.driver [req-b0f9d3a5-273c-401d-9c61-f84cf0a91b18] [instance: 2e0f047e-8d2f-4166-a950-ecf24c8743a7] Deleting instance files /opt/stack/data/nova/instances/2e0f047e-8d2f-4166-a950-ecf24c8743a7_del",
+                    "INFO nova.virt.libvirt.driver [req-b0f9d3a5-273c-401d-9c61-f84cf0a91b18] [instance: 2e0f047e-8d2f-4166-a950-ecf24c8743a7] Deletion of /opt/stack/data/nova/instances/2e0f047e-8d2f-4166-a950-ecf24c8743a7_del complete",                                        
+                    "INFO nova.compute.manager [req-ee9a4f4d-45b6-47cf-9541-92f8433b3592] [instance: 2e0f047e-8d2f-4166-a950-ecf24c8743a7] Took 58.84 seconds to build instance.",
+                    "INFO nova.compute.manager [req-b0f9d3a5-273c-401d-9c61-f84cf0a91b18] [instance: 2e0f047e-8d2f-4166-a950-ecf24c8743a7] Terminating instance",
+                    "INFO nova.compute.manager [req-b0f9d3a5-273c-401d-9c61-f84cf0a91b18] [instance: 2e0f047e-8d2f-4166-a950-ecf24c8743a7] Took 4.51 seconds to destroy the instance on the hypervisor.",
+                    "INFO nova.compute.manager [req-37b90fcc-54bf-416b-a83c-8a6c5f6cb55d req-6c5f6906-0acc-490e-ba57-44946ef7c57d service nova] [instance: 2e0f047e-8d2f-4166-a950-ecf24c8743a7] Neutron deleted interface 1e0088b3-9b3f-45e6-aeba-639a7208fdc8; detaching it from the instance and deleting it from the info cache",
+                    "INFO nova.compute.claims [req-ee9a4f4d-45b6-47cf-9541-92f8433b3592] [instance: 2e0f047e-8d2f-4166-a950-ecf24c8743a7] Claim successful on node parisaserver"
+                ],
+                "description": "Real log sequence from nova-sample.log showing instance lifecycle",
+                "input_type": "raw_logs"
+            }
+        },
+        {
+            "name": "Normal instance lifecycle",
+            "data": {
+                "event_sequence": [
+                    "INFO nova.compute.claims [req-e665dbf5-dcc9-468d-bc98-eb9aaf8071c6] [instance: b9f0f7cd-31c1-4edf-9a8d-129d4efff387] Claim successful on node parisaserver",
+                    "INFO nova.virt.libvirt.driver [req-e665dbf5-dcc9-468d-bc98-eb9aaf8071c6] [instance: b9f0f7cd-31c1-4edf-9a8d-129d4efff387] Creating image",
+                    "INFO os_vif [req-e665dbf5-dcc9-468d-bc98-eb9aaf8071c6] Successfully plugged vif VIFOpenVSwitch",
+                    "INFO nova.compute.manager [req-ac9f5721-5c52-4ec3-ba8a-e494d9780d53] [instance: b9f0f7cd-31c1-4edf-9a8d-129d4efff387] VM Started (Lifecycle Event)",
+                    "INFO nova.virt.libvirt.driver [-] [instance: b9f0f7cd-31c1-4edf-9a8d-129d4efff387] Instance spawned successfully.",
+                    "INFO nova.compute.manager [req-e665dbf5-dcc9-468d-bc98-eb9aaf8071c6] [instance: b9f0f7cd-31c1-4edf-9a8d-129d4efff387] Took 9.31 seconds to spawn the instance on the hypervisor."
+                ],
+                "description": "Normal instance creation and startup sequence",
+                "input_type": "raw_logs"
+            }
+        },
+        {
+            "name": "Instance termination sequence",
+            "data": {
+                "event_sequence": [
+                    "INFO nova.compute.manager [req-979f4d38-e788-4abf-ab98-5d63ce15f517] [instance: 11760334-ac63-4cc8-9086-578422af8c99] Terminating instance",
+                    "INFO nova.virt.libvirt.driver [-] [instance: 11760334-ac63-4cc8-9086-578422af8c99] Instance destroyed successfully.",
+                    "INFO os_vif [req-979f4d38-e788-4abf-ab98-5d63ce15f517] Successfully unplugged vif VIFOpenVSwitch",
+                    "INFO nova.virt.libvirt.driver [req-979f4d38-e788-4abf-ab98-5d63ce15f517] [instance: 11760334-ac63-4cc8-9086-578422af8c99] Deleting instance files /opt/stack/data/nova/instances/11760334-ac63-4cc8-9086-578422af8c99_del",
+                    "INFO nova.compute.manager [req-979f4d38-e788-4abf-ab98-5d63ce15f517] [instance: 11760334-ac63-4cc8-9086-578422af8c99] Took 4.14 seconds to destroy the instance on the hypervisor.",
+                    "INFO nova.scheduler.client.report [req-979f4d38-e788-4abf-ab98-5d63ce15f517] Deleted allocation for instance 11760334-ac63-4cc8-9086-578422af8c99"
+                ],
+                "description": "Normal instance termination and cleanup sequence",
+                "input_type": "raw_logs"
+            }
+        },
+        {
+            "name": "VM lifecycle with pause resume",
+            "data": {
+                "event_sequence": [
+                    "INFO nova.compute.manager [req-ac9f5721-5c52-4ec3-ba8a-e494d9780d53] [instance: 506a23f5-6cec-4d64-abf8-16e017154255] VM Started (Lifecycle Event)",
+                    "INFO nova.compute.manager [req-ac9f5721-5c52-4ec3-ba8a-e494d9780d53] [instance: 506a23f5-6cec-4d64-abf8-16e017154255] VM Paused (Lifecycle Event)",
+                    "INFO nova.compute.manager [req-ac9f5721-5c52-4ec3-ba8a-e494d9780d53] [instance: 506a23f5-6cec-4d64-abf8-16e017154255] During sync_power_state the instance has a pending task (spawning). Skip.",
+                    "INFO nova.compute.manager [req-ac9f5721-5c52-4ec3-ba8a-e494d9780d53] [instance: 506a23f5-6cec-4d64-abf8-16e017154255] VM Resumed (Lifecycle Event)",
+                    "INFO nova.virt.libvirt.driver [-] [instance: 506a23f5-6cec-4d64-abf8-16e017154255] Instance spawned successfully.",
+                    "INFO nova.compute.manager [req-01e56c59-0677-42e5-ba82-405497483d8e] [instance: 506a23f5-6cec-4d64-abf8-16e017154255] Took 82.50 seconds to spawn the instance on the hypervisor."
+                ],
+                "description": "VM lifecycle with pause and resume events",
+                "input_type": "raw_logs"
+            }
+        },
+        {
+            "name": "Libvirt domain error sequence",
+            "data": {
+                "event_sequence": [
+                    "INFO nova.compute.manager [req-2d399014-32df-490c-97a4-646ba82df5cf] [instance: f52f599a-b2a2-478c-bcfe-9933528dd526] Terminating instance",
+                    "INFO nova.virt.libvirt.driver [-] [instance: f52f599a-b2a2-478c-bcfe-9933528dd526] Instance destroyed successfully.",
+                    "INFO nova.compute.manager [req-2d399014-32df-490c-97a4-646ba82df5cf] [instance: f52f599a-b2a2-478c-bcfe-9933528dd526] Took 3.86 seconds to destroy the instance on the hypervisor.",
+                    "WARNING nova.virt.libvirt.driver [req-32833659-9280-430c-b915-a246f788ad28] Error from libvirt while getting description of instance-00000072: [Error Code 42] Domain not found: no domain with matching uuid 'f52f599a-b2a2-478c-bcfe-9933528dd526' (instance-00000072): libvirt.libvirtError: Domain not found",
+                    "INFO nova.compute.manager [-] [instance: f52f599a-b2a2-478c-bcfe-9933528dd526] VM Stopped (Lifecycle Event)",
+                    "INFO nova.scheduler.client.report [req-2d399014-32df-490c-97a4-646ba82df5cf] Deleted allocation for instance f52f599a-b2a2-478c-bcfe-9933528dd526"
+                ],
+                "description": "Instance termination with libvirt domain not found warning",
+                "input_type": "raw_logs"
+            }
+        },
+        {
+            "name": "Power state synchronization warning",
+            "data": {
+                "event_sequence": [
+                    "INFO nova.compute.manager [req-77396c32-0c2f-4d85-bb19-7174aa911288] [instance: 2f093863-6416-4d09-ac00-7ea45d932d36] Took 82.81 seconds to spawn the instance on the hypervisor.",
+                    "INFO nova.compute.manager [req-ac9f5721-5c52-4ec3-ba8a-e494d9780d53] [instance: 2f093863-6416-4d09-ac00-7ea45d932d36] VM Resumed (Lifecycle Event)",
+                    "INFO nova.compute.manager [req-77396c32-0c2f-4d85-bb19-7174aa911288] [instance: 2f093863-6416-4d09-ac00-7ea45d932d36] Took 93.32 seconds to build instance.",
+                    "WARNING nova.compute.manager [req-32833659-9280-430c-b915-a246f788ad28] While synchronizing instance power states, found 9 instances in the database and 8 instances on the hypervisor.",
+                    "INFO nova.virt.libvirt.driver [req-e9767169-a830-4f59-a660-0d28173bec12] [instance: e4faecd7-54e4-470b-83af-8c021fb864d7] Creating image"
+                ],
+                "description": "Power state synchronization issue between database and hypervisor",
+                "input_type": "raw_logs"
+            }
+        },
+        {
+            "name": "Disk storage error sequence",
+            "data": {
+                "event_sequence": [
+                    "INFO nova.compute.manager [req-ac9f5721-5c52-4ec3-ba8a-e494d9780d53] [instance: 8505e839-8c3f-40a0-8eff-41c36ba17e9d] VM Started (Lifecycle Event)",
+                    "INFO nova.virt.libvirt.driver [-] [instance: 8505e839-8c3f-40a0-8eff-41c36ba17e9d] Instance spawned successfully.",
+                    "WARNING nova.virt.libvirt.driver [req-32833659-9280-430c-b915-a246f788ad28] Periodic task is updating the host stats, it is trying to get disk info for instance-00000090, but the backing disk storage was removed by a concurrent operation such as resize. Error: No disk at /opt/stack/data/nova/instances/8505e839-8c3f-40a0-8eff-41c36ba17e9d/disk: nova.exception.DiskNotFound",
+                    "WARNING nova.compute.manager [req-32833659-9280-430c-b915-a246f788ad28] While synchronizing instance power states, found 9 instances in the database and 7 instances on the hypervisor.",
+                    "INFO nova.compute.manager [req-1234abcd-5678-90ef-abcd-1234567890ab] [instance: 8505e839-8c3f-40a0-8eff-41c36ba17e9d] Terminating instance"
+                ],
+                "description": "Disk storage issue during host stats update with power state mismatch",
+                "input_type": "raw_logs"
+            }
         }
-        # ,
-        # {
-        #     "name": "Empty sequence (should fail)",
-        #     "data": {
-        #         "event_sequence": [],
-        #         "description": "Empty sequence test"
-        #     }
-        # }
     ]
     
     print(f"Testing sequence prediction endpoint at {BASE_URL}{ENDPOINT}")
--- uiUtilities.py
+++ uiUtilities.py
@@ -6,6 +6,7 @@ import sys
 from data_process import parser as data_process_parser, mapping as data_process_mapping, deeplog_sampling as data_process_deeplog_sampling, generate_train_test as data_process_generate_train_test, run_data_processing_pipeline
 from data_process_pred import prePredict_process, parser as pred_parser
 from deeplog import predict, process_vocab
+from logparser import Drain
 
 def ui_train(input_dir='./trainInput/', output_dir='./trainOutput/', log_file="nova-sample-training.log", log_format='<Level> <Component> <ADDR> <Content>'):
 
@@ -77,7 +78,7 @@ def ui_predict_sequence(event_sequence):
     Returns:
         Dictionary with prediction result and confidence
     """
-    try:
+    try:    
         print(f"Predicting anomaly for sequence Test: {event_sequence}")
         
         # Import required modules for single sequence prediction
@@ -89,12 +90,22 @@ def ui_predict_sequence(event_sequence):
         import numpy as np
         import tempfile
         import os
+        
+        # Validate required options
+        required_options = ['input_size', 'hidden_size', 'num_layers', 'vocab_size', 'embedding_dim', 'model_path', 'device', 'window_size', 'num_candidates', 'vocab_path']
+        missing_options = [opt for opt in required_options if opt not in options]
+        if missing_options:
+            raise ValueError(f"Missing required options: {missing_options}")
+        
         # Log only the option parameters used by the model in the next line
         model_keys = ['input_size', 'hidden_size', 'num_layers', 'vocab_size', 'embedding_dim', 'model_path', 'device']
         print("Deeplog model options used for model initialization:")
         for k in model_keys:
             print(f"  {k}: {options.get(k)}")
         print(f"Using model path: {options['model_path']}")
+        print(f"Using vocab path: {options['vocab_path']}")
+        print(f"Window size: {options['window_size']}")
+        print(f"Num candidates: {options['num_candidates']}")
         # Load the trained model
         model = Deeplog(options['input_size'], 
                      options['hidden_size'], 
@@ -128,6 +139,14 @@ def ui_predict_sequence(event_sequence):
             # Already numeric IDs
             sequence_ids = event_sequence
         
+        print(f"Raw sequence_ids: {sequence_ids}")
+        print(f"Vocab size from options: {options.get('vocab_size', 'Not found')}")
+        
+        # Validate and clamp sequence IDs to vocab size
+        vocab_size = options.get('vocab_size', 100)  # Default fallback
+        sequence_ids = [min(max(int(sid), 0), vocab_size - 1) for sid in sequence_ids]
+        print(f"Validated sequence_ids: {sequence_ids}")
+        
         # Ensure sequence has minimum length
         if len(sequence_ids) < options['window_size']:
             # Pad with zeros if sequence is too short
@@ -139,24 +158,45 @@ def ui_predict_sequence(event_sequence):
         
         for i in range(len(sequence_ids) - options['window_size'] + 1):
             window = sequence_ids[i:i + options['window_size']]
-            
+            print(f"Processing window {i + 1}/{len(sequence_ids) - options['window_size'] + 1}: {window}")
             # Convert to tensor
             input_seq = torch.tensor(window[:-1], dtype=torch.long).unsqueeze(0).to(options['device'])
+            print(f"Input sequence tensor: {input_seq}")
             target = torch.tensor([window[-1]], dtype=torch.long).to(options['device'])
-            
+            print(f"Target tensor: {target}")
             with torch.no_grad():
                 output = model([input_seq], options['device'])
+                print(f"Model output shape: {output.shape}")
+                print(f"Output tensor: {output}")
+                
+                # Validate output dimensions
+                if len(output.shape) != 2:
+                    raise ValueError(f"Expected 2D output tensor, got shape: {output.shape}")
                 
                 # Get top candidates for prediction
                 predicted_candidates = torch.argsort(output, descending=True)[0][:options['num_candidates']]
+                print(f"Predicted candidates: {predicted_candidates}")
                 
-                # Check if actual next event is in top candidates
-                is_anomaly = target[0] not in predicted_candidates
+                # Validate target index is within vocab size
+                target_idx = target[0].item()
+                vocab_size = output.shape[1]
+                print(f"Target index: {target_idx}, Vocab size: {vocab_size}")
                 
-                # Calculate anomaly score (inverse of prediction probability)
-                probs = torch.softmax(output, dim=1)
-                target_prob = probs[0][target[0]].item()
-                anomaly_score = 1 - target_prob
+                if target_idx >= vocab_size:
+                    print(f"Warning: Target index {target_idx} is out of range for vocab size {vocab_size}")
+                    # Use a safe fallback
+                    is_anomaly = True
+                    anomaly_score = 1.0
+                else:
+                    # Check if actual next event is in top candidates
+                    is_anomaly = target[0] not in predicted_candidates
+                    
+                    # Calculate anomaly score (inverse of prediction probability)
+                    probs = torch.softmax(output, dim=1)
+                    target_prob = probs[0][target_idx].item()
+                    anomaly_score = 1 - target_prob
+                
+                print(f"Window {i}: Predicted candidates: {predicted_candidates}, Target: {target[0]}, Anomaly: {is_anomaly}")
                 
                 predictions.append(is_anomaly)
                 anomaly_scores.append(anomaly_score)
@@ -202,17 +242,32 @@ def _parse_log_sequence_to_ids(log_lines):
         # Create temporary files for processing
         with tempfile.NamedTemporaryFile(mode='w', suffix='.log', delete=False) as temp_log:
             for line in log_lines:
+                print(f"line from log {line}")
                 temp_log.write(line.strip() + '\n')
             temp_log_path = temp_log.name
         
-        temp_output_dir = tempfile.mkdtemp()
+        temp_output_dir = os.path.join(os.path.dirname(__file__), "temp_log_parse_output")
+        os.makedirs(temp_output_dir, exist_ok=True)
         
         try:
             # Initialize Drain parser with same settings as training
             log_format = '<Level> <Component> <ADDR> <Content>'
-            parser = LogParser(log_format, indir=os.path.dirname(temp_log_path), 
-                             outdir=temp_output_dir, depth=5, st=0.4)
-            
+            # parser = LogParser(log_format, indir=os.path.dirname(temp_log_path), 
+            #                  outdir=temp_output_dir, depth=5, st=0.4)
+            regex = [
+            #r"[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12}", #compute instance id e.g. a208479c-c0e3-4730-a5a0-d75e8afd0252
+            #r'^[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{12},\d+,[a-fA-F0-9]+,[a-fA-F0-9]{32} [a-fA-F0-9]{32} (?:- ){3}-\]$',  # represents - 4b0c1f2e-3d5a-4c8b-9f6d-7e8f9a0b1c2d,34,462cb051,4b0c1f2e3d5a4c8b9f6d7e8f9a0b1c2d - - - -]'
+            # in Creating event network-vif-plugged:a208479c-c0e3-4730-a5a0-d75e8afd0252 for instance 96abccce-8d1f-4e07-b6d1-4b2ab87e23b4,111,2da4176f,f7b8d1f1d4d44643b07fa10ca7d021fb e9746973ac574c6b8a9e8857f56a7608 - - -] 
+			r'^[a-z0-9.-]+,\d+,[a-fA-F0-9]+,(?:- ){4}-\]$', # represents - cp-1.slowvm1.tcloud-pg0.utah.cloudlab.us,34,462cb051,- - - - -] 
+            r'[a-z0-9.-]+(?:\.[a-z0-9-]+)+',
+            r"[a-f0-9]{8}-[a-f0-9]{4}-[1-5][a-f0-9]{3}-[89ab][a-f0-9]{3}-[a-f0-9]{12}",  #compute instance id e.g. a208479c-c0e3-4730-a5a0-d75e8afd0252
+            r"(/[-\w]+)+",  # file path
+            r'\d+\.\d+\.\d+\.\d+',  # IP
+            r"(/[-\w]+)+ HTTP/1.1",  # file path with HTTP protocol
+            r'\d+(\.\d+)?',  # Numbers with optional decimal point
+            ]
+            parser = Drain.LogParser(log_format, indir=os.path.dirname(temp_log_path), outdir=temp_output_dir, depth=10, st=0.5, rex=regex, keep_para=False)
+
             # Parse the logs
             log_file = os.path.basename(temp_log_path)
             parser.parse(log_file)
@@ -226,12 +281,19 @@ def _parse_log_sequence_to_ids(log_lines):
                 templates_file = './output/deeplog/nova-sample.log_templates.csv'
                 if os.path.exists(templates_file):
                     templates_df = pd.read_csv(templates_file)
+                    
                     template_to_id = dict(zip(templates_df['EventId'], templates_df['templateIndex']))
                     
                     # Map EventIds to numeric IDs
                     event_ids = []
                     for event_id in df['EventId']:
-                        numeric_id = template_to_id.get(event_id, 0)  # Default to 0 for unknown
+                        print(f"event_id from df {event_id}")
+                        if event_id in template_to_id:
+                            numeric_id = template_to_id[event_id]
+                            print(f"Mapped {event_id} to {numeric_id}")
+                        else:
+                            numeric_id = 0  # Default to 0 for unknown
+                        #numeric_id = template_to_id.get(event_id, 0)  # Default to 0 for unknown
                         event_ids.append(numeric_id)
                     
                     return event_ids
